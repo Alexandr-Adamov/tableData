b9596a6850d33c92d84989cbe1cb250bc9c943c6
a.adamov <wof_4@mail.ru>
2021-02-16 12:46:24 +0300

    Updates

---
+++ precache-manifest.e29d820e19aaaa7683c2b2b4ed764044.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d64f2476f095fe36ac54d82380253a",
+    "url": "/table-data/index.html"
+  },
+  {
+    "revision": "05ccee20708cae792f6e",
+    "url": "/table-data/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "58fb0a95de6a1ee6c599",
+    "url": "/table-data/static/css/main.626cd379.chunk.css"
+  },
+  {
+    "revision": "05ccee20708cae792f6e",
+    "url": "/table-data/static/js/2.94d0830f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/table-data/static/js/2.94d0830f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58fb0a95de6a1ee6c599",
+    "url": "/table-data/static/js/main.82dd8c69.chunk.js"
+  },
+  {
+    "revision": "271a94e9e20dad6c078c",
+    "url": "/table-data/static/js/runtime-main.d3287b15.js"
+  },
+  {
+    "revision": "27fbf4eb5aff7ff361ddaa24ca7e0478",
+    "url": "/table-data/static/media/nothing-found (1).27fbf4eb.png"
+  }
+]);
